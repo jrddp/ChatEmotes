4be728366f247db4ffbb6db19e330fe1043f5442
Jared Peters <jar3dpt@gmail.com>
2020-04-09 07:13:02 -0400

    Initial commit

---
+++ src/main/java/me/kokeria/chatemotes/ChatEmotes.java
@@ -0,0 +1,19 @@
+package me.kokeria.chatemotes;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ChatEmotes.MODID, version = ChatEmotes.VERSION)
+public class ChatEmotes
+{
+    public static final String MODID = "chatemotes";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+}
