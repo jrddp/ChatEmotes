5fc9501bf229d17199459079f03f842953f11b3c
Jared Peters <jar3dpt@gmail.com>
2020-04-24 15:16:37 -0400

    Add command

--- src/main/java/me/kokeria/chatemotes/ChatEmotes.java
+++ src/main/java/me/kokeria/chatemotes/ChatEmotes.java
@@ -2,13 +2,16 @@ package me.kokeria.chatemotes;
 
 import me.kokeria.chatemotes.util.FileHandler;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.registry.CommandRegistry;
 
 public class ChatEmotes implements ClientModInitializer {
 
+    // todo add button for modlist to go to config file
     @Override
     public void onInitializeClient() {
 
         FileHandler.setEmotesFromFile();
+        CommandRegistry.INSTANCE.register(false, EmoteCommand::register);
 
     }
 }
---
+++ src/main/java/me/kokeria/chatemotes/EmoteCommand.java
@@ -0,0 +1,57 @@
+package me.kokeria.chatemotes;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import me.kokeria.chatemotes.util.ChatHelper;
+import me.kokeria.chatemotes.util.FileHandler;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.*;
+import net.minecraft.util.Formatting;
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class EmoteCommand {
+
+    static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+
+        dispatcher.register(CommandManager.literal("emote").executes(EmoteCommand::execute));
+
+    }
+
+    private static int execute(CommandContext<?> ctx) {
+
+        FileHandler.setEmotesFromFile();
+
+        Map<String, String> emotes = FileHandler.getEMOTES();
+        Iterator<String> iterator = emotes.keySet().iterator();
+
+        if (!iterator.hasNext()) {
+            Style style = new Style().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, FileHandler.KEYFILE.getAbsolutePath()));
+            style.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new LiteralText("Click to open config file") {
+            }));
+            ChatHelper.addMsgToChat(new LiteralText(Formatting.RED + "You have no emotes registered!").setStyle(style));
+            return 1;
+        }
+
+        ChatHelper.addMsgToChat(Formatting.GREEN + "Type an emote in chat and hit tab to replace!");
+
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            String emote = emotes.get(key);
+            String msg = Formatting.GOLD + key + Formatting.WHITE + " - " + Formatting.YELLOW + emote;
+            Text component = new LiteralText(msg);
+            Style style = new Style().setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, emote));
+            style.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new LiteralText("Click to set chat to " + emote)));
+            component.setStyle(style);
+            ChatHelper.addMsgToChat(component);
+        }
+
+        return 1;
+
+    }
+
+}
+
+
